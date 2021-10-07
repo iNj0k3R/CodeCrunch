9796b060ce3eefb93275db899aad6e449e3e1085
varad <varadsoham555@gmail.com>
2021-10-07 19:02:27 +0530

    NASA Section error handling

--- Controllers/NASAController.cs
+++ Controllers/NASAController.cs
@@ -1,5 +1,6 @@
 ﻿using CodeCrunch.Services.NASA;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace CodeCrunch.Controllers
 {
@@ -16,47 +17,114 @@ namespace CodeCrunch.Controllers
         [HttpGet("image-of-month")]
         public async Task<IActionResult> ImageOfMonthAsync()
         {
-            DateTime date = DateTime.Now;
-            var image = await _NASAService.GetAPOD(date.ToString("yyyy-MM-01"));
-            var response = new
+            try
             {
-                date = image.date,
-                media_type = image.media_type,
-                title = image.title,
-                url = image.url
-            };
-            return Ok(response);
+                DateTime date = DateTime.Now;
+                var image = await _NASAService.GetAPOD(date.ToString("yyyy-MM-01"));
+                var response = new
+                {
+                    date = image.date,
+                    media_type = image.media_type,
+                    title = image.title,
+                    url = image.url
+                };
+                return Ok(response);
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode((int)ex.StatusCode,
+                    new
+                    {
+                        status = (int)ex.StatusCode,
+                        message = "image/video not found"
+                    });
+            }          
+            
         }
         [HttpGet("images-of-month/{year}/{month}")]
         public async Task<IActionResult> ImagesOfMonthAsync(string year, string month)
         {
-            var APOD = await _NASAService.GetAPODOfMonth(year, month);
-            var response = APOD.Select(t => t.url);
-            return Ok(response);
+            try
+            {
+                var APOD = await _NASAService.GetAPODOfMonth(year, month);
+                var response = APOD.Select(t => t.url);
+                return Ok(response);
+            }
+            catch (FormatException ex)
+            {
+                return BadRequest();
+       
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode((int)ex.StatusCode,
+                    new
+                    {
+                        status = (int)ex.StatusCode,
+                        message = "image/video not found"
+                    });
+            }
+           
         }
         [HttpGet("videos-of-month/{year}/{month}")]
         public async Task<IActionResult> VideosOfMonthAsync(string year, string month)
         {
-            var APOD = await _NASAService.GetAPODOfMonth(year, month);
-            var response = APOD.Where(t => t.media_type is "video").Select(t => t.url);
-            return Ok(response);
+            try
+            {
+                var APOD = await _NASAService.GetAPODOfMonth(year, month);
+                var response = APOD.Where(t => t.media_type is "video").Select(t => t.url);
+                return Ok(response);
+            }
+            catch (FormatException ex)
+            {
+                return BadRequest();
+
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode((int)ex.StatusCode, new
+                {
+                    status = (int)ex.StatusCode,
+                    message = "image/video not found"
+                });
+            }
         }
         [HttpGet("earth-poly-image/{date}")]
         public async Task<IActionResult> EPICAsync(string date)
         {
-            var EPIC = await _NASAService.GetEPIC(date);
-            var latLonFiltered = EPIC.Where(t => t.centroid_coordinates.lat >= 10 && t.centroid_coordinates.lat <= 40
-                 && t.centroid_coordinates.lon >= 120 && t.centroid_coordinates.lon <= 160);
-            var response = latLonFiltered.Select(t => new
+            try
             {
-                identifier = t.identifier,
-                caption = t.caption,
-                image = t.image,
-                date = t.date,
-                latitude = t.centroid_coordinates.lat,
-                longitude = t.centroid_coordinates.lon,
-            });
-            return Ok(response);
+                var EPIC = await _NASAService.GetEPIC(date);
+                if(EPIC.Length <= 0)
+                {
+                    return NotFound(new
+                    {
+                        status = 404,
+                        message = "image/video not found"
+                    });
+                }
+                var latLonFiltered = EPIC.Where(t => t.centroid_coordinates.lat >= 10 && t.centroid_coordinates.lat <= 40
+                     && t.centroid_coordinates.lon >= 120 && t.centroid_coordinates.lon <= 160);
+                var response = latLonFiltered.Select(t => new
+                {
+                    identifier = t.identifier,
+                    caption = t.caption,
+                    image = t.image,
+                    date = t.date,
+                    latitude = t.centroid_coordinates.lat,
+                    longitude = t.centroid_coordinates.lon,
+                });
+                return Ok(response);
+            }
+
+            catch (HttpRequestException ex)
+            {
+                return StatusCode((int)ex.StatusCode, new
+                {
+                    status = (int)ex.StatusCode,
+                    message = "image/video not found"
+                });
+            }
         }
     }
 }
--- Services/NASA/NASAService.cs
+++ Services/NASA/NASAService.cs
@@ -20,16 +20,24 @@ namespace CodeCrunch.Services.NASA
 
         public Task<APOD[]> GetAPODOfMonth(string year, string month)
         {
-            DateTime date = DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture);
-            var firstDayOfMonth = new DateTime(Int32.Parse(year),date.Month,1);
-            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
-            string sDate = year + "-" + date.Month + "-" + firstDayOfMonth.Day;
-            string lDate = year + "-" + date.Month + "-" + lastDayOfMonth.Day;
-            return _clientFactory.CreateClient().GetFromJsonAsync<APOD[]>($"https://api.nasa.gov/planetary/apod?start_date={sDate}&end_date={lDate}&api_key={API_KEY}");
+            try
+            {
+                DateTime date = DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture);
+                var lastDayOfMonth = date.AddMonths(1).AddDays(-1);
+                string sDate = year + "-" + date.Month + "-" + date.Day;
+                string lDate = year + "-" + date.Month + "-" + lastDayOfMonth.Day;
+                return _clientFactory.CreateClient().GetFromJsonAsync<APOD[]>($"https://api.nasa.gov/planetary/apod?start_date={sDate}&end_date={lDate}&api_key={API_KEY}");
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+            
         }
 
         public Task<EPIC[]> GetEPIC(string date)
         {
+            
             return _clientFactory.CreateClient().GetFromJsonAsync<EPIC[]>($"https://api.nasa.gov/EPIC/api/natural/date/{date}?api_key={API_KEY}");
         }
     }
